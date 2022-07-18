39e5a98b7ebcf10518d43a6d9dc1e8e5b54d5c31
tigran.tskanyan <tigran.tskanyan@spbtv.com>
2022-07-18 11:16:46 +0300

    updat webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -15,15 +15,34 @@ const isProd = !isDev;
 const globals = getGlobals();
 const optimization = () => {
   const config = {
+    minimize: isProd,
     splitChunks: {
-      chunks: "all",
+      chunks: "async",
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
   };
 
   if (isProd) {
     config.minimizer = [
       new CssMinimizerWebpackPlugin(),
-      new TerserWebpackPlugin(),
+      new TerserWebpackPlugin({ parallel: true }),
     ];
   }
 
